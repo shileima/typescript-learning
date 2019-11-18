bf1343e3cba4a127a49e8585dc2fed375868345f
loading <604237073@qq.com>
2019-11-18 19:34:39 +0800

    接口与装饰器

--- hello.ts
+++ hello.ts
@@ -1,6 +1,30 @@
+/* readyonly */
+class Anima {
+    constructor(public readonly name: string) { }
+    getName() {
+        console.log(this.name);
+    }
+}
+let ani = new Anima('penny')
+// ani.name = 'dd' // readonly 属性不能重新赋值
+console.log(ani.name);
+
+class Dog extends Anima {
+    constructor(name: string, age: number) {
+        super(name)
+    }
+}
+let d = new Dog('rrre', 12)
+d.getName()
+console.log('---------------------');
+
 /* 存取器 */
 class User {
-    myname: string;
+    //myname: string;
+    // public 设置参数为具有全局属性
+    constructor(public myname: string) {
+        // this.myname = myname;
+    }
     get name() {
         console.log('get')
         return this.myname;
@@ -10,10 +34,10 @@ class User {
         this.myname = newName;
     }
 }
-let u = new User();
+//let u = new User(123);
+let u = new User('loading');
 u.myname = "seanma";
 console.log(u);
-console.log('---------------------');
 /* 类 */
 class Person {
     name: string;
@@ -40,12 +64,6 @@ attr('loading')
 // attr(true)
 attr(10)
 console.log(obj);
-
-function sum(...number: number[]) {
-    console.log(Array.isArray(number))
-    return number.reduce((current, item) => current + item, 0)
-}
-console.log(sum(1, 2, 3, 4));
 
 type getUsername = (x: string, y: string) => void; // string
 let getUsernameFunction: getUsername = function (firstname: string, lastname: string) {
@@ -95,3 +113,64 @@ bb()
 let arr: number | (number | string)[] = [12, 12, 23, 32, "loading"] // 普通数组
 let arr2: [string, number, boolean] = ["age", 12, false] // 元数组必须成对出现
 console.log(arr2);
+
+// 可选参数：在TS中函数的形参和实参必须一样，不一样就要配置可选参数,而且必须是最后一个参数
+function print(name: string, age?: number): void {
+    console.log(name, age);
+}
+print('zfpx');
+
+// 默认参数
+function ajax(url: string, method: string = 'GET') {
+    console.log(url, method);
+}
+ajax('/users');
+
+// 剩余参数
+function sum(...number: number[]) {
+    console.log(Array.isArray(number))
+    return number.reduce((current, item) => current + item, 0)
+}
+console.log(sum(12, 12));
+
+// 属性存取器
+class User2 {
+    //类里面可以访问， 子类和其它任何地方都不可以访问
+    private mynamePrivate: string;
+    //类里面 子类 其它任何地方外边都可以访问
+    protected mynameProtected: string;
+    public mynamePublic: string;
+    //类里面 子类 都可以访问,其它任何地方不能访问
+    constructor(public myname: string) {
+        this.mynamePublic = myname;
+        this.mynamePrivate = myname;
+    }
+    get h() {
+        console.log('get')
+        return this.mynamePublic;
+    }
+    set h(value) {
+        console.log('set')
+        this.mynamePublic = value;
+    }
+}
+
+let user = new User2('zhufeng');
+class UserCopy extends User2 {
+    constructor(public myname: string) {
+        super(myname)
+        this.mynamePublic = myname;
+        this.mynameProtected = myname;
+        // this.mynamePrivate = myname;
+    }
+}
+let usercopy = new UserCopy('userCopy')
+user.h = 'jiagou';
+console.log(user.h);
+console.log(user.mynamePublic)
+// console.log(user.mynameProtected)
+// console.log(user.mynamePrivate)
+
+console.log(usercopy.h)
+console.log(usercopy.mynamePublic)
+// console.log(usercopy.mynameProtected)
