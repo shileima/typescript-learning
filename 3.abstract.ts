bca6a2ff5c3d09f1cb480d7c7cc041570536bdb4
loading <604237073@qq.com>
2019-11-20 15:09:33 +0800

    泛型

--- 3.abstract.ts
+++ 3.abstract.ts
@@ -46,10 +46,7 @@ c2.getName()
  * 接口内的所有方法都为抽象
  * 接口是为了实现某些身份或功能例如 fly eat
  */
-interface Animal2 {
-    name: string
-    getName(): any
-}
+
 interface Speakable {
     name: string
     speak(): void
@@ -65,13 +62,69 @@ let speakman: Speakable, Speakable2 = {
     }
 }
 
+// 任意属性
+interface Animal2 {
+    readonly name: string;
+    getName(): string;
+    [propName: string]: any
+}
+let a2: Animal2 = {
+    name: 'Jones',
+    getName() { return this.name },
+    age: 10,
+    city: 'beijing'
+}
 
+// 接口继承
+interface SpeakableParent {
+    speak(): void
+}
+interface SpeakableUncle {
+    say(): void
+}
+interface SpeakableChild extends SpeakableParent, SpeakableUncle {
+    speakChinese(): void
+}
+class Man implements SpeakableChild {
+    speakChinese() { console.log('Chinese') }
+    speak() { console.log('speak') }
+    say() { }
+}
+// 函数类型接口
+interface Discount {
+    (price: number, discount: number): number
+}
+let cost: Discount = function (price: number): number {
+    return price * 0.9
+}
+console.log(cost(10, 0.8))
 
+// 可索引接口
+interface UserInterface {
+    [aaa: number]: boolean | string
+}
+let arr1: UserInterface = ['1', '2', '3', true]
+let obj1: UserInterface = { 0: false, 1: '1' }
 
+// 约束类的构造函数
 
+class Animal3 {
+    constructor(public name: string) {
+    }
+}
+class Animal4 {
+    constructor() {
 
-
-
+    }
+}
+interface withClassName {
+    new(name: string): Animal3
+}
+function create(clazz: withClassName, name: string) {
+    return new clazz(name)
+}
+let c1 = create(Animal3, "loading")
+console.log(c1.name)
 
 
 
